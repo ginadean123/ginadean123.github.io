4c14ba7900a90c947442aef54b00808424fb0ed5
Gina Dean <ginad@ziprecruiter.com>
2025-08-29 10:10:22 -0400

    repo name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/<repo-name>/',
+  base: '/ginadean123.github.io/',
   plugins: [react()],
 })
